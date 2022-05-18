b3778632d3538b18e3c9d0d8c0b945ce03923cb0
ouko <oukoezzy@gmail.com>
2022-05-18 19:55:13 +0300

    ezra changes

--- execute.c
+++ execute.c
@@ -1,24 +1,48 @@
-#include "shell.h"
-
+#include "main.h"
 /**
- * execute_prog - Executes binary or builtin
- * @args: Argument buffer
- * @line: Line buffer
- * @env: Environment
- * @flow: Helps decide what code to run
- * Return: 1 if success
+ * execute_proc - similar to puts in C
+ * @cmd: a pointer the integer we want to set to 402
+ *
+ * Return: int
  */
-int execute_prog(char **args, char *line, char **env, int flow)
+void execute_proc(char **cmd)
 {
-	int status;
 
-	if (args[0] == NULL)
-		return (1);
-	if (flow == 1)
+	char *parametro = (*(cmd + 1));
+	char *s, *slash = "/";
+	char *o;
+
+	char *vartoprint = *cmd;
+	char *argv[4];
+
+	if ((access(cmd[0], F_OK) == 0))
 	{
-		if (check_for_builtins(args, line, env) == 1)
-			return (1);
+		argv[0] = cmd[0];
+		argv[1] = parametro;
+		argv[2] = ".";
+		argv[3] = NULL;
+
+		if (execve(argv[0], argv, NULL) == -1)
+		{
+			perror("Error");
+		}
 	}
-	status = launch_prog(args);
-	return (status);
+	else
+	{
+		o = find_command(vartoprint);
+
+		slash = str_concat(o, slash);
+
+		s = str_concat(slash, *cmd);
+
+		argv[0] = s;
+		argv[1] = parametro;
+		argv[2] = ".";
+		argv[3] = NULL;
+
+		if (execve(argv[0], argv, NULL) == -1)
+		{
+			perror("Error");
+		}
+	}
 }
