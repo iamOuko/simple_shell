10611ca460d02f057a9868ae232b5f7e26687e77
mellanieachieng <mellanie.achieng@gmail.com>
2022-05-18 17:39:18 +0000

    Simple shell updated

---
+++ get_env.c
@@ -0,0 +1,52 @@
+#include "shell.h"
+/**
+ * cmp_env_name - compares env variables names
+ * with the name passed.
+ * @nenv: name of the environment variable
+ * @name: name passed
+ *
+ * Return: 0 if are not equal. Another value if they are.
+ */
+int cmp_env_name(const char *nenv, const char *name)
+{
+	int i;
+
+	for (i = 0; nenv[i] != '='; i++)
+	{
+		if (nenv[i] != name[i])
+		{
+			return (0);
+		}
+	}
+
+	return (i + 1);
+}
+/**
+ * _getenv - get an environment variable
+ * @name: Name of the variable to find
+ * @_environ: environment variable
+ * Return: Pointer with the value of the variable.
+ */
+char *_getenv(char *name, char **_environ)
+{
+	char *ptr_env;
+	int i, mov;
+
+	/* Initialize ptr_env value */
+	ptr_env = NULL;
+	mov = 0;
+	/* Compare all environment variables */
+	/* environ is declared in the header file */
+	for (i = 0; _environ[i]; i++)
+	{
+		/* If name and env are equal */
+		mov = cmp_env_name(_environ[i], name);
+		if (mov)
+		{
+			ptr_env = _environ[i];
+			break;
+		}
+	}
+
+	return (ptr_env + mov);
+}
---
+++ shell.h
@@ -0,0 +1,26 @@
+#ifndef SHELL
+#define SHELL
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <string.h>
+#include <errno.h>
+
+extern char **environ;
+
+int _strlen(const char *s);
+char *_strcpy(char *dest, const char *src);
+int _putchar(char c);
+void _puts(char *str);
+unsigned int nbr_spaces(char *s);
+char **stringToTokens(char *str);
+char *_strcat(char *dest, char *src);
+int _strcmp(char *s1, char *s2);
+char *_strtok(char *s, char d);
+char *_strdup(char *str);
+char *_getenv(char *name, char **_environ);
+
+#endif
