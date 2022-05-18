76eab86800271ab117fb51248c578bc1d6adadf2
ouko <oukoezzy@gmail.com>
2022-05-18 21:27:41 +0300

    changes

--- shell.c
+++ shell.c
@@ -1,47 +1,118 @@
 #include "shell.h"
-/**
- * shell - Infinite loop that runs shell
- * @ac: Arg count
- * @av: args passed to shell at beginning of prog
- * @env: Environment
- * Return: Void
- */
-void shell(int ac, char **av, char **env)
-{
-	char *line;
-	char **args;
-	int status = 1;
-	char *tmp = NULL;
-	char *er;
-	char *filename;
-	int flow;
+#include <string.h>
 
-	er = "Error";
-	do {
-		prompt();
-		line = _getline();
-		args = split_line(line);
-		flow = bridge(args[0], args);
-		if (flow == 2)
+/**
+ * execute - executes the command
+ * @cmd: command to run
+ * @main_path: path to environment variable
+ * Return: 0 on success1 -1 if cmd is exit and 1 on any other error
+ */
+int execute(char **cmd, char *main_path)
+{
+	pid_t child_pid;
+	int status;
+
+	child_pid = fork();
+	if (child_pid == -1)
+	{
+		perror("Error");
+		return (1);
+	}
+	else if (child_pid == 0)
+	{
+		if (**(cmd + 0) == '/')
 		{
-			filename = args[0];
-			args[0] = find_path(args[0], tmp, er);
-			if (args[0] == er)
+			execve(*(cmd + 0), cmd, environ);
+			if (execve(*(cmd + 0), cmd, environ) == -1)
 			{
-				args[0] = search_cwd(filename, er);
-				if (args[0] == filename)
-					write(1, er, 5);
+				perror("./shell");
+				exit(1);
 			}
 		}
-		if (args[0] != er)
-			status = execute_prog(args, line, env, flow);
-		free(line);
-		free(args);
-	} while (status);
-	if (!ac)
-		(void)ac;
-	if (!av)
-		(void)av;
-	if (!env)
-		(void)env;
+		else
+		{
+			_strcat(main_path, "/");
+			_strcat(main_path, cmd[0]);
+			execve(main_path, cmd, environ);
+			if (execve(main_path, cmd, environ) == -1)
+			{
+				perror("./shell");
+				exit(1);
+			}
+		}
+	}
+	else
+		wait(&status);
+
+	return (0);
+}
+
+/**
+ *get_path - function to get the path environment variable
+ *@path: environment variable
+ *Return: 0
+ */
+char *get_path(char *path)
+{
+	char *main_path;
+	char *cutpath;
+
+	cutpath = strtok(path, ":");
+	while (cutpath != NULL)
+	{
+		if (!_strcmp(cutpath, "/usr/bin"))
+			main_path = cutpath;
+		cutpath = strtok(NULL, ":");
+	}
+	return (main_path);
+}
+
+
+/**
+ * main - main simple shell
+ * @argc: number of arguments
+ * @argv: list of command line arguments
+ * Return: Always 0, -1 on error.
+ */
+
+int main(int argc __attribute__((unused)), char **argv __attribute__((unused)))
+{
+	int response, character, isPipe = 0;
+	size_t bufsize = BUFSIZ;
+	char **tokens = (char **)malloc(sizeof(char **) * bufsize);
+	char *buffer, *path;
+	char *main_path = (char *)malloc(sizeof(char *) * bufsize);
+
+	buffer = (char *)malloc(sizeof(char *) * bufsize);
+	if (buffer == NULL)
+	{
+		perror("Unable to allocate buffer");
+	}
+	path = _getenv("PATH", environ);
+	main_path = get_path(path);
+	do {
+		if (isatty(fileno(stdin)))
+		{
+			isPipe = 1;
+			_puts("user@dexter$ ");
+		}
+		character = getline(&buffer, &bufsize, stdin);
+		buffer[_strlen(buffer) - 1] = '\0';
+		if (!_strcmp("exit", buffer) || character == EOF)
+		{
+			free(buffer);
+			free(tokens);
+			exit(1);
+		}
+		tokens = stringToTokens(buffer);
+		response = execute(tokens, main_path);
+	} while (isPipe && response != -1);
+	if (isPipe && response == -1)
+	{
+		free(buffer);
+		free(main_path);
+	}
+	free(buffer);
+	free(tokens);
+	return (0);
 }
---
+++ strings.c
@@ -0,0 +1,90 @@
+#include "shell.h"
+
+/**
+ * _strlen - find the length of a string
+ * @s: pointer to the string to check
+ * Return: void
+ */
+
+
+int _strlen(const char *s)
+{
+	int i = 0;
+
+	while (s[i])
+		i++;
+
+	return (i);
+}
+
+
+/**
+ * *_strcpy -  copies the string pointed to by src
+ * @dest: char type string
+ * @src: char type string
+ * Description: Copy the string pointed to by pointer `src` to
+ * the buffer pointed to by `dest`
+ * Return: Pointer to `dest`
+ */
+
+char *_strcpy(char *dest, const char *src)
+{
+	int i = -1;
+
+	do {
+		i++;
+		dest[i] = src[i];
+	} while (src[i] != '\0');
+
+	return (dest);
+}
+/**
+ * _strcat - concatenates two strings
+ * @dest: input value
+ * @src: input value
+ *
+ * Return: void
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i;
+	int j;
+
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	j = 0;
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+/**
+ * _strcmp - compare string values
+ * @s1: input value
+ * @s2: input value
+ *
+ * Return: s1[i] - s2[i]
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	i = 0;
+	while (s1[i] != '\0' && s2[i] != '\0')
+	{
+		if (s1[i] != s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
+		i++;
+	}
+	return (0);
+}
